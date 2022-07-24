38a81ac330f265c1bed7254df6cb26457b28bca3
Leonardo13344 <lrsuniga@uce.edu.ec>
2022-07-24 18:55:50 -0500

    Disparos Flechas

--- Assets/Scripts/PersonajeMovement.cs
+++ Assets/Scripts/PersonajeMovement.cs
@@ -7,8 +7,6 @@ public class PersonajeMovement : MonoBehaviour
     private Rigidbody2D Rigidbody2D;
     private float Horizontal;
     private float vertical;
-    private float arrowHorizontal;
-    private float arrowVertical;
     public GameObject BulletPrefab;
     private float LastShoot;
     public float speed;
@@ -22,35 +20,46 @@ public class PersonajeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         Horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        
         
         if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         if (vertical < 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         else if (vertical > 0.0f) transform.localScale = new Vector3(1.0f, -1.0f, 1.0f);
-        if (Input.GetKey(KeyCode.Space) && Time.time > LastShoot + 0.25f)
+        
+        if (Input.GetKey(KeyCode.UpArrow) && Time.time > LastShoot + 0.25f)
         {
-            shoot();
+            shoot(Vector2.up);
+            LastShoot = Time.time;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && Time.time > LastShoot + 0.25f)
+        {
+            shoot(Vector2.down);
+            LastShoot = Time.time;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && Time.time > LastShoot + 0.25f)
+        {
+            shoot(Vector2.left);
+            LastShoot = Time.time;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && Time.time > LastShoot + 0.25f)
+        {
+            shoot(Vector2.right);
             LastShoot = Time.time;
         }
     }
 
-    
-    private void shoot()
+
+    private void shoot(Vector3 dir)
     {
-        Vector3 direction;
-        if (transform.localScale.x == 1.0f && transform.localScale.y == 1) direction = Vector2.right;
-        else if (transform.localScale.x == -1.0f && transform.localScale.y == 1) direction = Vector2.left;
-        else if (transform.localScale.y == -1.0f && transform.localScale.x == 1) direction = Vector2.up;
-        else if (transform.localScale.y == 1.0f && transform.localScale.x == 1.0f) direction = Vector2.down;
-        else direction = Vector2.right;
-        
-        GameObject bullet = Instantiate(BulletPrefab, transform.position + direction * 0.1f, Quaternion.identity);
-        bullet.GetComponent<bulletScript>().setDirection(direction);
+        GameObject bullet = Instantiate(BulletPrefab, transform.position + dir * 0.1f, Quaternion.identity);
+        bullet.GetComponent<bulletScript>().setDirection(dir);
     }
 
     private void FixedUpdate(){
-        Rigidbody2D.velocity = new Vector2(Horizontal, vertical);
+        Rigidbody2D.velocity = new Vector2(Horizontal, vertical);   
     }
 }
