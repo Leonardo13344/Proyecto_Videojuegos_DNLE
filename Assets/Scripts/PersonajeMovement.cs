b0ee6c0d34eb2bda342863377b052fb00f168ff8
Leonardo13344 <lrsuniga@uce.edu.ec>
2022-07-25 20:15:15 -0500

    Animaciones direcciones

--- Assets/Scripts/PersonajeMovement.cs
+++ Assets/Scripts/PersonajeMovement.cs
@@ -7,6 +7,7 @@ public class PersonajeMovement : MonoBehaviour
     private Rigidbody2D Rigidbody2D;
     private float Horizontal;
     private float vertical;
+    private Animator animator;
     public GameObject BulletPrefab;
     private float LastShoot;
     public float speed;
@@ -15,6 +16,7 @@ public class PersonajeMovement : MonoBehaviour
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,31 +25,53 @@ public class PersonajeMovement : MonoBehaviour
         
         Horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+        animator.SetBool("running", Horizontal != 0.0f);
+        animator.SetBool("runningDown", vertical < 0.0f);
+        animator.SetBool("runningUp", vertical > 0.0f);
+
+        //Movimiento
+        if (Horizontal < 0.0f)
+        {
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
+        if (Horizontal > 0.0f)
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+        if (vertical < 0.0f)
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            
+        }
+        if (vertical > 0.0f)
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
         
-        
-        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        if (vertical < 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        else if (vertical > 0.0f) transform.localScale = new Vector3(1.0f, -1.0f, 1.0f);
-        
+        //Disparos
         if (Input.GetKey(KeyCode.UpArrow) && Time.time > LastShoot + 0.25f)
         {
             shoot(Vector2.up);
+            //transform.localScale = new Vector3(1.0f,-1.0f,1.0f);
             LastShoot = Time.time;
         }
         if (Input.GetKey(KeyCode.DownArrow) && Time.time > LastShoot + 0.25f)
         {
             shoot(Vector2.down);
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
             LastShoot = Time.time;
         }
         if (Input.GetKey(KeyCode.LeftArrow) && Time.time > LastShoot + 0.25f)
         {
             shoot(Vector2.left);
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
             LastShoot = Time.time;
         }
         if (Input.GetKey(KeyCode.RightArrow) && Time.time > LastShoot + 0.25f)
         {
             shoot(Vector2.right);
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
             LastShoot = Time.time;
         }
     }
